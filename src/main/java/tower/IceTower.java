3e0f38e6adf112e6fc1f638fa2bcdaee7215b43f
Happy <babeabanban@gmail.com>
2019-11-29 21:56:42 +0900

    New TestCase

--- src/main/java/tower/IceTower.java
+++ src/main/java/tower/IceTower.java
@@ -40,6 +40,7 @@ public class IceTower extends Tower{
     @Override
     public boolean isInRange(Location monsterLoc) {
         double distance = Math.sqrt((loc.x - monsterLoc.x) * (loc.x - monsterLoc.x) + (loc.y - monsterLoc.y) * (loc.y - monsterLoc.y));
+        System.out.println(distance);
         return (distance <= range);
     }
 
