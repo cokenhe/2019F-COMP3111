a5ab98e3b70c1eceaea2e2b0c85f679ecd793df1
Happy <babeabanban@gmail.com>
2019-11-29 16:39:03 +0900

    Tower Update (all basic function without value)

--- src/main/java/tower/LaserTower.java
+++ src/main/java/tower/LaserTower.java
@@ -6,22 +6,48 @@ import monster.Monster;
     abstract public class LaserTower extends Tower{
         
         public static final int BUILDCOST = 0;
+        public int energy = 100;
+        public boolean isRecharging = false;
+        public int[] consumeRate = {100, 100, 100, 50 ,50};
 
         LaserTower(int x, int y){
             attackPower = 1;
-            range = 65;
             upgradeCost = 1;   
             loc = new Location(x,y);
         }
 
-        public void attack(Monster monsters){
-
+        public void recharge(){
+            if (energy == 0)
+                isRecharging = true;
+            if (isRecharging)
+                energy += 20;
         }
 
+        @Override
         public void upgrade(){
-
+            if (level >= 5) return;
+            level++;
+            attackPower++; 
         }
 
+        @Override
+        public boolean isInRange(Location monsterLoc) {
+            return true;
+        }
+
+        @Override
+        public Monster findNearestMonster(Monster[] monsters) {
+            if (!isRecharging){
+                Monster selectedMonster = super.findNearestMonster(monsters);
+                if (selectedMonster != null)
+                    energy -= consumeRate[level-1];
+                return selectedMonster;
+            }else{
+                recharge();
+                return null;
+            }
+
+        }
     }
 
   
